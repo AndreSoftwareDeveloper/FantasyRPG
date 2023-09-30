a1c98a63e1019e772dc0e0ef7bb06275a8fb0479
AndreSoftwareDeveloper <andriej2301@gmail.com>
2023-09-30 11:24:43 +0200

    fix: Sword attact animation now works properly.

--- Assets/MovementStates/MovementStateManager.cs
+++ Assets/MovementStates/MovementStateManager.cs
@@ -30,37 +30,31 @@ public class MovementStateManager : MonoBehaviour
     public WalkState Walk = new WalkState();
     public CrouchState Crouch = new CrouchState();
     public RunState Run = new RunState();
-    public AttackState Attack = new AttackState();
 
     [HideInInspector] public Animator anim;
 
-    void Start()
-    {
+    void Start() {
         anim = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
-        SwitchState(Idle);
+        SwitchState(Idle);        
     }
 
-    void Update()
-    {
+    void Update() {
         GetDirectionAndMove();
-        SwordAttack();
         Gravity();
+        SwordAttack();
 
         anim.SetFloat("hzInput", hzInput);
         anim.SetFloat("vInput", vInput);
-
-        currentState.UpdateState(this);
+        currentState.UpdateState(this);        
     }
 
-    public void SwitchState(MovementBaseState state)
-    {
+    public void SwitchState(MovementBaseState state) {
         currentState = state;
         currentState.EnterState(this);
     }
 
-    void GetDirectionAndMove()
-    {
+    void GetDirectionAndMove() {
         hzInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
 
@@ -69,15 +63,14 @@ public class MovementStateManager : MonoBehaviour
         controller.Move(dir.normalized * currentMoveSpeed * Time.deltaTime);
     }
 
-    bool IsGrounded()
-    {
+    bool IsGrounded() {
         spherePos = new Vector3(transform.position.x, transform.position.y - groundYOffset, transform.position.z);
-        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
+        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask))
+            return true;
         return false;
     }
 
-    void Gravity()
-    {
+    void Gravity() {
         if (!IsGrounded()) 
             velocity.y += gravity * Time.deltaTime;
         else if (velocity.y < 0) 
@@ -86,13 +79,11 @@ public class MovementStateManager : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-    void SwordAttack()
-    {
+    void SwordAttack() {
         if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("attack_debug");
-            SwitchState(Attack);
-        }
-            
+            anim.SetBool("Attacking", true);    // sword swing on mouse click
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") && (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.8f) )        
+            anim.SetBool("Attacking", false);   // idle pose after attack  
     }
 }
